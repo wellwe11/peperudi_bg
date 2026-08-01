d0eb43b1b1ba1fe1d6afe39b3e6dad8cf37d0ab5
wellwe11 <robin1ryan@hotmail.com>
2026-08-01 21:54:37 +0300

    updated styles & hero section

--- src/routes/index/route.tsx
+++ src/routes/index/route.tsx
@@ -1,4 +1,4 @@
-import logo from "@/resources/images/branding/logo.png";
+import heroImg from "@/resources/images/branding/hero_ex_one.jpg";
 
 import "./style.scss";
 import { createFileRoute } from "@tanstack/react-router";
@@ -16,22 +16,34 @@ function RouteComponent() {
 }
 
 const Hero = () => {
-  // Big picture right
-  // Big text left
-  // Very small text below
-  // same very small size text, but gray below small text
-
   return (
-    <div className="hero container">
+    <section className="hero container">
       {/**Left side */}
-      <div className="text_wrapper">{/** Will contain texts */}</div>
+      <div className="text_wrapper">
+        {/** Will contain texts */}
+
+        <h1 className="hero_name">
+          <span>Peperudi</span>
+          <span className="title_span">.BG</span>
+        </h1>
+
+        <h5 className="hero_caption">
+          <span className="eyebrow sans_text">
+            Butterflies · Scorpions · Spiders
+            <span>
+              <a>Sofia</a> · <a>Plovdiv</a> · <a>Varna</a>
+            </span>
+            <span className="s">
+              <span>Scroll</span>
+            </span>
+          </span>
+        </h5>
+      </div>
 
       {/**Right side */}
       <div className="image_wrapper">
-        {/**Big image */}
-
-        <img src={logo} alt="" />
+        <img src={heroImg} alt="eperudi BG logo" />
       </div>
-    </div>
+    </section>
   );
 };
